0144fb2cf8885dcef914a357b00fbb0fdcc6231b
RajPanjwani-2001 <69621432+RajPanjwani-2001@users.noreply.github.com>
2021-07-22 14:49:23 +0530

    Update settings.py

--- stocks/settings.py
+++ stocks/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mp8#x3gq6&&#st06-nhz^3zd6$%89afv867w&cttb7plfs^@ua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','travelloraj.herokuapp.com']
 
